8aa90400f0ea2e519e63fb0b611f137462b94b3c
yessky <admin@veryos.com>
2017-01-09 17:42:04 +0800

    auto create index file with gulp task

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var gutil = require('gutil');
+var del = require('del');
+var through2 = require('through2');
+var sequence = require('run-sequence');
+
+var isEnded = false;
+
+gulp.task('default', function(cb) {
+	var counter = 0;
+	gulp.src([
+		// 'index.tpl',
+		'./test-*.html'
+	], {read: false})
+	.pipe(through2.obj(function(file, enc, done) {
+		this.push(file);
+		counter++;
+		done();
+		console.log(file.path)
+	}))
+	.pipe(gulp.dest('./'))
+	.on('end', onReady);
+
+	function onReady() {
+		gulp.src('index.tpl')
+			.pipe(through2.obj(function(file, enc, done) {
+				var tpl = file.contents + '';
+				var content = '';
+				for (var i = 1; i <= counter; ++i) {
+					var idx = i < 10 ? '0' + i : i;
+					content += '\n\t<p><a href="test-' + idx + '.html" target="_blank">Chapter ' + idx + '</a></p>';
+				}
+				content += '\n';
+				file.path = file.path.replace('.tpl', '.html');
+				file.contents = new Buffer(tpl.replace('$$CONTENT$$', content));
+				this.push(file);
+				done();
+			}))
+			.pipe(gulp.dest('./'))
+			.on('end', cb);
+	}
+});
---
+++ index.tpl
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>canvas starter lessons</title>
+  <style type="text/css">
+  body {font-size: 14px; line-height: 125%; color: #333;}
+  a {text-decoration: none}
+  a:hover { text-decoration: underline}
+  </style>
+</head>
+
+<body>$$CONTENT$$</body>
+
+</html>
